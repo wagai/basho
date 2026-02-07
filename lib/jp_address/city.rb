15e3a65c7ffc2c08a207fd49498aae212ee6dabe
Hirotaka Wagai <hirotaka.wagai@gmail.com>
2026-02-08 02:01:23 +0900

    refactor: CityをData.defineイミュータブル値オブジェクトに変換
    - 手動attr_readerからData.defineブロック構文へ
    - capital?を1行メソッド定義に簡略化
    - **匿名ダブルスプラットでbare superを使用

--- lib/jp_address/city.rb
+++ lib/jp_address/city.rb
@@ -1,28 +1,20 @@
 # frozen_string_literal: true
 
 module JpAddress
-  class City
-    attr_reader :code, :prefecture_code, :name, :name_kana
-
-    def initialize(code:, prefecture_code:, name:, name_kana:, capital: false)
-      @code = code
-      @prefecture_code = prefecture_code
-      @name = name
-      @name_kana = name_kana
-      @capital = capital
+  City = ::Data.define(:code, :prefecture_code, :name, :name_kana, :capital) do
+    def initialize(capital: false, **)
+      super
     end
 
-    def capital?
-      @capital
-    end
+    def capital? = capital
 
     def prefecture
-      Prefecture.find(@prefecture_code)
+      Prefecture.find(prefecture_code)
     end
 
     class << self
       def find(code)
-        return nil unless code.is_a?(String) && code.length == 6
+        return nil unless code.is_a?(String) && code.size == 6
 
         prefecture_code = code[0..1].to_i
         where(prefecture_code: prefecture_code).find { |city| city.code == code }
