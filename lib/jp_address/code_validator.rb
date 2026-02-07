6913842903c04af4f2a8773ffb48ce96a3e067f5
Hirotaka Wagai <hirotaka.wagai@gmail.com>
2026-02-08 02:01:43 +0900

    refactor: CodeValidatorのバリデーションをRubyイディオムに改善
    - 文字列長チェックを正規表現/\A\d{6}\z/に変更
    - チェックディジット比較を文字列から整数に変更

--- lib/jp_address/code_validator.rb
+++ lib/jp_address/code_validator.rb
@@ -4,22 +4,21 @@ module JpAddress
   module CodeValidator
     CHECK_DIGITS_INDEX = 5
     CHECK_BASE = 11
-    VALID_CODE_LENGTH = 6
     PREFECTURE_RANGE = (1..47)
 
     module_function
 
     def valid?(code)
-      return false unless code.is_a?(String) && code.length == VALID_CODE_LENGTH
+      return false unless code.is_a?(String) && code.match?(/\A\d{6}\z/)
       return false unless PREFECTURE_RANGE.cover?(code[0..1].to_i)
 
-      code[CHECK_DIGITS_INDEX] == compute_check_digit(code).to_s
+      code[CHECK_DIGITS_INDEX].to_i == compute_check_digit(code)
     end
 
     def compute_check_digit(code)
       sub_total = code.chars
                       .take(CHECK_DIGITS_INDEX)
-                      .map.with_index { |digit, index| digit.to_i * (CHECK_DIGITS_INDEX - index + 1) }
+                      .map.with_index { |digit, i| digit.to_i * (CHECK_DIGITS_INDEX - i + 1) }
                       .sum
 
       candidate = (CHECK_BASE - (sub_total % CHECK_BASE)) % 10
