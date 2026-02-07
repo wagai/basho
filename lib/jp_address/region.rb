77ad9ddb0f31f449b7c03b4df76f9b1890ff84fd
Hirotaka Wagai <hirotaka.wagai@gmail.com>
2026-02-08 02:00:53 +0900

    refactor: RegionをData.defineイミュータブル値オブジェクトに変換
    - Hashデータ定数+手動attr_readerからData.defineブロック構文へ
    - REGIONS定数をclass << self内のメモ化allメソッドに移動
    - Lint/ConstantDefinitionInBlock違反を解消

--- lib/jp_address/region.rb
+++ lib/jp_address/region.rb
@@ -1,33 +1,23 @@
 # frozen_string_literal: true
 
 module JpAddress
-  class Region
-    attr_reader :name, :name_en, :prefecture_codes
-
-    REGIONS = [
-      { name: "北海道", name_en: "Hokkaido", prefecture_codes: [1] },
-      { name: "東北", name_en: "Tohoku", prefecture_codes: [2, 3, 4, 5, 6, 7] },
-      { name: "関東", name_en: "Kanto", prefecture_codes: [8, 9, 10, 11, 12, 13, 14] },
-      { name: "中部", name_en: "Chubu", prefecture_codes: [15, 16, 17, 18, 19, 20, 21, 22, 23] },
-      { name: "近畿", name_en: "Kinki", prefecture_codes: [24, 25, 26, 27, 28, 29, 30] },
-      { name: "中国", name_en: "Chugoku", prefecture_codes: [31, 32, 33, 34, 35] },
-      { name: "四国", name_en: "Shikoku", prefecture_codes: [36, 37, 38, 39] },
-      { name: "九州", name_en: "Kyushu", prefecture_codes: [40, 41, 42, 43, 44, 45, 46, 47] }
-    ].freeze
-
-    def initialize(name:, name_en:, prefecture_codes:)
-      @name = name
-      @name_en = name_en
-      @prefecture_codes = prefecture_codes
-    end
-
+  Region = ::Data.define(:name, :name_en, :prefecture_codes) do
     def prefectures
-      @prefecture_codes.map { |code| Prefecture.find(code) }
+      prefecture_codes.map { |code| Prefecture.find(code) }
     end
 
     class << self
       def all
-        @all ||= REGIONS.map { |data| new(**data) }
+        @all ||= [
+          new(name: "北海道", name_en: "Hokkaido", prefecture_codes: [1]),
+          new(name: "東北", name_en: "Tohoku", prefecture_codes: [2, 3, 4, 5, 6, 7]),
+          new(name: "関東", name_en: "Kanto", prefecture_codes: [8, 9, 10, 11, 12, 13, 14]),
+          new(name: "中部", name_en: "Chubu", prefecture_codes: [15, 16, 17, 18, 19, 20, 21, 22, 23]),
+          new(name: "近畿", name_en: "Kinki", prefecture_codes: [24, 25, 26, 27, 28, 29, 30]),
+          new(name: "中国", name_en: "Chugoku", prefecture_codes: [31, 32, 33, 34, 35]),
+          new(name: "四国", name_en: "Shikoku", prefecture_codes: [36, 37, 38, 39]),
+          new(name: "九州", name_en: "Kyushu", prefecture_codes: [40, 41, 42, 43, 44, 45, 46, 47])
+        ].freeze
       end
 
       def find(name)
